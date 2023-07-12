f43971d9c0f32fadbac516d960e9514bd65c5970
azizul-orvee <orvee16@gmail.com>
2023-07-12 23:18:43 +0600

    revert here

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Branch Main
+          Branch Main with deleted files
         </a>
       </header>
     </div>
